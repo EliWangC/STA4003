f087f3dd3797f3b07c4f560a98a8e17e4d9b5026
Eli Wang <117010258@link.cuhk.edu.cn>
2021-05-12 23:22:28 +0800

    Implemented recursive multi-step forecast

--- Project-v1.R
+++ Project-v1.R
@@ -27,33 +27,32 @@ index.test.Step3 <- (n1+3):(n1+n2)    # From week 211 to week 260
 MSE <- rep(0,3)                       # The mean squared errors for 1-Step, 2-Step, 3-Step forecasts
 MSE.Stock <- matrix(rep(0,(p-1)*3),3,(p-1)) # MSE for each stock and h-step forecast
 
-# ------------------------------------------------- #
-##### My code for manipulating the data around and see
-##### what does the data look like
-
-# Assign a to S&P500 data for simplicity
-SP500 = StockData[, 1]
-
-# See what our time series data (the log returns) looks like
-plot(SP500, type = "l", xlab = "Week", ylab = "Log Returns (S&P500)")
-plot(StockData[, 7], type = "l", xlab = "Week", ylab = "Log Returns (Stock 6)")
-
-# Conduct ADF test for dataset
-print(adf.test(SP500))
-
-# Plot ACF and PACF
-acf(SP500, main = "ACF Plot (S&P500)")
-pacf(SP500, main = "PACF Plot (S&P500)")
-
-# We apply auto-arima model to the dataset
-sp500_train = SP500[1:n1]
-sp500_test = SP500[(n1+1):(n1+n2)]
-sp500fit <- auto.arima(sp500_train, lambda = "auto")
-plot(resid(sp500fit), type = "l", xlab = "Week", ylab = "Model Residuals (S&P500)")
-
-sp500_forecast <- forecast(sp500fit, h = 4)
-plot(sp500_forecast)
-# ------------------------------------------------- #
+# # ------------------------------------------------- #
+# ##### My code for manipulating the data around and see what does the data look like
+# 
+# # Assign a to S&P500 data for simplicity
+# SP500 = StockData[, 1]
+# 
+# # See what our time series data (the log returns) looks like
+# plot(SP500, type = "l", xlab = "Week", ylab = "Log Returns (S&P500)")
+# plot(StockData[, 7], type = "l", xlab = "Week", ylab = "Log Returns (Stock 6)")
+# 
+# # Conduct ADF test for dataset
+# print(adf.test(SP500))
+# 
+# # Plot ACF and PACF
+# acf(SP500, main = "ACF Plot (S&P500)")
+# pacf(SP500, main = "PACF Plot (S&P500)")
+# 
+# # We apply auto-arima model to the dataset
+# sp500_train = SP500[1:n1]
+# sp500_test = SP500[(n1+1):(n1+n2)]
+# sp500fit <- auto.arima(sp500_train, lambda = "auto")
+# plot(resid(sp500fit), type = "l", xlab = "Week", ylab = "Model Residuals (S&P500)")
+# 
+# sp500_forecast <- forecast(sp500fit, h = 4)
+# plot(sp500_forecast)
+# # ------------------------------------------------- #
 
 ########## Your code to build the time series model  
 start.time <- Sys.time()  # Start measuring the execution time
@@ -67,17 +66,27 @@ for (j in 2:p)      # For stock 1 to 10
   
   for (i in 1:n2)   # For week i in the test set
   {
-    # index.train <- i:(i+n1-1)  # Moving training windows
-    index.train <- 1:n1
-    #### Your code for building model ####
-    # Fit the AUTO-ARIMA model that automatically selects the best model parameters 
-    arima_model <- auto.arima(Stock[index.train], lambda = "auto")
-    arima_forecast <- forecast(arima_model, h = n2)
+    # Moving training windows
+    index.train <- i:(i+n1-1) 
+    train_set <- Stock[index.train]
+    predictions <- c()
+    
+    ##### Your code for building model #####
+    ### Recursive Multi-step Forecast ###
+    for (h in 1:3) 
+    { 
+      # Fit the AUTO-ARIMA model that automatically selects the best model parameters 
+      arima_model <- auto.arima(train_set, lambda = "auto")
+      arima_forecast <- forecast(arima_model, h = 1)
+      onestep_forecast <- arima_forecast$mean[1]             # Get the predicted value
+      predictions <- c(predictions, onestep_forecast)        # Store the prediction value
+      train_set <- c(train_set, onestep_forecast)            # Add the predicted value into the training set for recursive forecast
+    }
     
     # Your h-step forecast for Stock j on Week i, h = 1, 2, or 3.
-    step1.forecasts[i] <- arima_forecast$mean[i]
-    step2.forecasts[i] <- arima_forecast$mean[i+1]
-    step3.forecasts[i] <- arima_forecast$mean[i+2]
+    step1.forecasts[i] <- predictions[1]
+    step2.forecasts[i] <- predictions[2]
+    step3.forecasts[i] <- predictions[3]
   }
   
   MSE.Stock[1, (j-1)] <- sum((Stock[index.test.Step1] - step1.forecasts)^2)
@@ -86,15 +95,15 @@ for (j in 2:p)      # For stock 1 to 10
 }
 
 
-######## Your code for Trading stratedy
-### Example: My trading strategy is buy and sell each stock with same weight
+##### Your code for Trading stratedy #####
+# Example: My trading strategy is buy and sell each stock with same weight
 return.stock <- rowMeans(StockData[(n1+1):(n1+n2),2:p])
 
 
 # End time measurement and show it
 end.time <- Sys.time()
 time.taken <- end.time - start.time
-cat("Runtime:", time.taken, "\n")
+cat("Model Runtime:", time.taken, "\n")
 
 ######### Output. Don't change any codes below.
 MSE <- rowMeans(MSE.Stock)
